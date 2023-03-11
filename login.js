bb837128a1d106442d9801f08d92d3956836cfd2
Tweagan11 <123520140+Tweagan11@users.noreply.github.com>
2023-03-10 22:58:57 -0700

    Murdered so much but did so much

--- login.js
+++ login.js
@@ -1,4 +1,4 @@
-class User {
+class User{
     onlineStatus;
 
     constructor(username, password) {
@@ -10,47 +10,45 @@ class User {
 
 }
 
-const users = [];
-
 cleanUsers();
 
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
-loginButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    
-    const username = loginForm.username.value;
-    const password = loginForm.password.value;
+if(loginButton){
+    loginButton.addEventListener("click", (e) => {
+        e.preventDefault();
+        
+        const username = loginForm.username.value;
+        const password = loginForm.password.value;
 
-    let users = [];
-    const usersText = localStorage.getItem('Users');
-    if (usersText) {
-        users = JSON.parse(usersText);
-    };
+        let users = [];
+        const usersText = localStorage.getItem('Users');
+        if (usersText) {
+            users = JSON.parse(usersText);
+        };
 
-    let test = users.find(User => User.username === username)
+        let test = users.find(User => User.username === username)
 
-    if (test) {
-        alert("You have successfully logged in.");
-        for(let status of Object.keys(users)) {
-            if(typeof User[status] == false && test.username) {
-                test.onlineStatus = true;
-            };
+        if (test) {
+            alert("You have successfully logged in.");
+            localStorage.setItem("currentUser", JSON.stringify(username));
+            const newCurrentUser = document.querySelector('#login-icon');
+            newCurrentUser.textContent = username;
+            location.reload();
+            cleanUsers();
+
+        } else if (username === "" || password === "") {
+            loginErrorMsg.style.opacity = 1;
+                loginErrorMsg.innerHTML = '<p>Please enter a Username and Password</p>';
+                
+        } else {
+            loginErrorMsg.style.opacity = 1;
         }
-        location.reload();
-        cleanUsers();
-
-    } else if (username === "" || password === "") {
-        loginErrorMsg.style.opacity = 1;
-            loginErrorMsg.innerHTML = '<p>Please enter a Username and Password</p>';
-            
-    } else {
-        loginErrorMsg.style.opacity = 1;
-    }
-       
-})
+        
+    })
+}
 
 function compareUser(user1, user2) {
     return user1.name === user2.name;
@@ -59,45 +57,50 @@ function compareUser(user1, user2) {
 const signupForm = document.getElementById("signup-form");
 const signupButton = document.getElementById("signup-form-submit");
 
-signupButton.addEventListener("click", (e) => {
-    e.preventDefault();
+if(signupButton){
+    signupButton.addEventListener("click", (e) => {
+        e.preventDefault();
 
-    const username = signupForm.newUsername.value;
-    const password = signupForm.newPassword.value;
-    const newUser = new User (username, password, onlineStatus = true);
+        const username = signupForm.newUsername.value;
+        const password = signupForm.newPassword.value;
+        const newUser = new User (username, password, onlineStatus = true);
 
-    let users = [];
-    const usersText = localStorage.getItem('Users');
-    if (usersText) {
-        users = JSON.parse(usersText);
-    }
+        let users = [];
+        const usersText = localStorage.getItem('Users');
+        if (usersText) {
+            users = JSON.parse(usersText);
+        }
 
-    let found = false;
+        let found = false;
 
-    let check = users.find(User => User.username === username);
+        let check = users.find(User => User.username === username);
 
-    if (username === "" || password === ""){
+        if (username === "" || password === ""){
 
-        loginErrorMsg.style.opacity = 1;
-            loginErrorMsg.innerHTML = '<p>Please enter a Username and Password</p>';
-        location.reload;
-        found = true;
-    }
-
-    if (check) {
             loginErrorMsg.style.opacity = 1;
-            loginErrorMsg.innerHTML = '<p>Already a User</p>';
+                loginErrorMsg.innerHTML = '<p>Please enter a Username and Password</p>';
+            location.reload;
             found = true;
-            }
+        }
 
-    if(!found) {
-        users.push(newUser);
-        localStorage.setItem('Users', JSON.stringify(users));
+        if (check) {
+                loginErrorMsg.style.opacity = 1;
+                loginErrorMsg.innerHTML = '<p>Already a User</p>';
+                found = true;
+                }
 
-        alert("You have successfully signed up!")
-        window.location.href = 'index.html'
-    }
-})
+        if(!found) {
+            users.push(newUser);
+            localStorage.setItem('Users', JSON.stringify(users));
+
+            alert("You have successfully signed up!");
+            localStorage.setItem("currentUser", JSON.stringify(username));
+            const newCurrentUser = document.querySelector('#login-icon');
+            newCurrentUser.textContent = username;
+            window.location.href = 'index.html';
+        }
+    })
+}
 
 function cleanUsers() {
     
